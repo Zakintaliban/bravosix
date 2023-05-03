bd637de748f735d338b5e0cef6edfbb6c7016ec6
Khasbullah Zakin <hasbizakin0804@gmail.com>
2023-05-03 21:36:39 +0700

    add test

---
+++ __test__/index.test.js
@@ -0,0 +1,39 @@
+const BravoSix = require("../index");
+
+describe("BravoSix", () => {
+  let bravoSix;
+  let targetElement;
+
+  beforeEach(() => {
+    targetElement = document.createElement("div");
+    bravoSix = new BravoSix(targetElement);
+  });
+
+  test("should set light-mode color theme", () => {
+    bravoSix.applyColors("light-mode");
+    expect(targetElement.style.backgroundColor).toBe("white");
+    expect(targetElement.style.color).toBe("black");
+  });
+
+  test("should set dark-mode color theme", () => {
+    bravoSix.applyColors("dark-mode");
+    expect(targetElement.style.backgroundColor).toBe("black");
+    expect(targetElement.style.color).toBe("white");
+  });
+
+  test("should set night-vision color theme", () => {
+    bravoSix.applyColors("night-vision");
+    expect(targetElement.style.backgroundColor).toBe("rgb(54, 121, 120)");
+    expect(targetElement.style.color).toBe("white");
+  });
+
+  test("should not set invalid color theme", () => {
+    const initialBackgroundColor = targetElement.style.backgroundColor;
+    const initialTextColor = targetElement.style.color;
+
+    bravoSix.applyColors("invalid-theme");
+
+    expect(targetElement.style.backgroundColor).toBe(initialBackgroundColor);
+    expect(targetElement.style.color).toBe(initialTextColor);
+  });
+});
