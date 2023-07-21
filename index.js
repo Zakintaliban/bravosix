50b06f0ba30f13d9ee3ab0324a9b375b62482113
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-07-21 16:49:38 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import { applyGoingDarkAnimation } from "./src/goingdark.js";
 import { getOriginalStyles, applyColors } from "./src/styles.js";
+import tombolCSS from "./src/tombol.js";
 
 class BravoSix {
   constructor(targetElement) {
     this.targetElement = targetElement || document.body;
     this.originalStyles = getOriginalStyles(this.targetElement);
+    this.applyButtonStyle();
+  }
+
+  applyButtonStyle() {
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = tombolCSS;
+    this.targetElement.appendChild(styleElement);
   }
 
   applyColors(colorTheme) {
--- src/tombol.js
+++ src/tombol.js
@@ -1,30 +1,27 @@
-const tombolCSS = `
-  /* tombol.css content here */
+function getButtonStyles() {
+  return `
   .tombol {
-    /* Styling button */
-    background-color: #0d161c;
-    color: #79d9ff;
-    padding: 8px 16px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 16px;
-    transition: all 0.4s ease-in-out;
+      /* Styling button */
+      background-color: #0d161c;
+      color: #79d9ff;
+      padding: 8px 16px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 16px;
+      transition: all 0.4s ease-in-out;
   }
 
   .tombol:hover {
-    background-color: #79d9ff;
-    color: #0d161c;
+      background-color: #79d9ff;
+      color: #0d161c;
   }
 
   .tombol:focus {
-    outline: none;
-    box-shadow: 0 0 0 3px rgba(121, 217, 255, 0.5);
+      outline: none;
+      box-shadow: 0 0 0 3px rgba(121, 217, 255, 0.5);
   }
-`;
+  `;
+}
 
-const styleElement = document.createElement("style");
-styleElement.innerHTML = tombolCSS;
-document.head.appendChild(styleElement);
-
-export default tombolCSS;
+export default getButtonStyles;
